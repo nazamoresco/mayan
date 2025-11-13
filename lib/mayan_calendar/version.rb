f2b3d531cac8f39a3b042a4e241ef29682bfd419
nazamoresco <nazamoresco99@gmail.com>
2025-11-12 21:50:17 -0300

    big bang

---
+++ lib/mayan_calendar/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MayanCalendar
+  VERSION = "0.1.0"
+end
---
+++ test/test_mayan_calendar.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestMayanCalendar < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::MayanCalendar::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
