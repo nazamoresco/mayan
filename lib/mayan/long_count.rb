02032b552897fdc303fbf70614e38a1d35302da2
nazamoresco <nazamoresco99@gmail.com>
2025-11-12 23:17:36 -0300

    define long_count::date

--- lib/mayan/long_count.rb
+++ lib/mayan/long_count.rb
@@ -3,6 +3,15 @@
 module Mayan
   class LongCount
     class Date
+      attr_reader :baktun, :katun, :tun, :winal, :kin
+
+      def initialize(baktun, katun, tun, winal, kin)
+        @baktun = baktun
+        @katun = katun
+        @tun = tun
+        @winal = winal
+        @kin = kin
+      end
     end
   end
 end
--- test/test_long_count.rb
+++ test/test_long_count.rb
@@ -10,4 +10,13 @@ class TestLongCount < Minitest::Test
   def test_that_date_class_exists
     assert defined?(Mayan::LongCount::Date)
   end
+
+  def test_date_initialization
+    date = Mayan::LongCount::Date.new(13, 0, 0, 0, 0)
+    assert_equal 13, date.baktun
+    assert_equal 0, date.katun
+    assert_equal 0, date.tun
+    assert_equal 0, date.winal
+    assert_equal 0, date.kin
+  end
 end
